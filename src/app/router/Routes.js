33ef2f6ae70918e35884a207b3d10bbf90534a8a
timofeyp <timofeyp@live.com>
2019-11-24 15:41:37 +0300

    add jsconfig.json absolute path

--- src/app/router/Routes.js
+++ src/app/router/Routes.js
@@ -9,49 +9,49 @@ import React from "react";
 import { Redirect, Route, Switch, withRouter } from "react-router-dom";
 import { shallowEqual, useSelector } from "react-redux";
 import { useLastLocation } from "react-router-last-location";
-import HomePage from "../pages/home/HomePage";
-import AuthPage from "../pages/auth/AuthPage";
-import ErrorsPage from "../pages/errors/ErrorsPage";
-import LogoutPage from "../pages/auth/Logout";
-import { LayoutContextProvider } from "../../_metronic";
-import * as routerHelpers from "../router/RouterHelpers";
+import HomePage from "app/pages/home/HomePage";
+import AuthPage from "app/pages/auth/AuthPage";
+import ErrorsPage from "app/pages/errors/ErrorsPage";
+import LogoutPage from "app/pages/auth/Logout";
+import { LayoutContextProvider } from "_metronic";
+import * as routerHelpers from "app/router/RouterHelpers";
 
 export const Routes = withRouter(({ Layout, history }) => {
-    const lastLocation = useLastLocation();
-    routerHelpers.saveLastLocation(lastLocation);
-    const { isAuthorized, menuConfig, userLastLocation } = useSelector(
-        ({ auth, urls, builder: { menuConfig } }) => ({
-            menuConfig,
-            isAuthorized: auth.user != null,
-            userLastLocation: routerHelpers.getLastLocation()
-        }),
-        shallowEqual
-    );
+  const lastLocation = useLastLocation();
+  routerHelpers.saveLastLocation(lastLocation);
+  const { isAuthorized, menuConfig, userLastLocation } = useSelector(
+    ({ auth, urls, builder: { menuConfig } }) => ({
+      menuConfig,
+      isAuthorized: auth.user != null,
+      userLastLocation: routerHelpers.getLastLocation()
+    }),
+    shallowEqual
+  );
 
-    return (
+  return (
     /* Create `LayoutContext` from current `history` and `menuConfig`. */
-        <LayoutContextProvider history={history} menuConfig={menuConfig}>
-            <Switch>
-                {!isAuthorized ? (
-                /* Render auth page when user at `/auth` and not authorized. */
-                    <Route path="/auth/login" component={AuthPage} />
-                ) : (
-                /* Otherwise redirect to root page (`/`) */
-                    <Redirect from="/auth" to={userLastLocation} />
-                )}
+    <LayoutContextProvider history={history} menuConfig={menuConfig}>
+      <Switch>
+        {!isAuthorized ? (
+        /* Render auth page when user at `/auth` and not authorized. */
+          <Route path="/auth/login" component={AuthPage} />
+        ) : (
+        /* Otherwise redirect to root page (`/`) */
+          <Redirect from="/auth" to={userLastLocation} />
+        )}
 
-                <Route path="/error" component={ErrorsPage} />
-                <Route path="/logout" component={LogoutPage} />
+        <Route path="/error" component={ErrorsPage} />
+        <Route path="/logout" component={LogoutPage} />
 
-                {!isAuthorized ? (
-                /* Redirect to `/auth` when user is not authorized */
-                    <Redirect to="/auth/login" />
-                ) : (
-                    <Layout>
-                        <HomePage userLastLocation={userLastLocation} />
-                    </Layout>
-                )}
-            </Switch>
-        </LayoutContextProvider>
-    );
+        {!isAuthorized ? (
+        /* Redirect to `/auth` when user is not authorized */
+          <Redirect to="/auth/login" />
+        ) : (
+          <Layout>
+            <HomePage userLastLocation={userLastLocation} />
+          </Layout>
+        )}
+      </Switch>
+    </LayoutContextProvider>
+  );
 });
